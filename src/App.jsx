2b0ec1f71a888b210be2c5a82668aa310a4813e2
rafael-gama <rafaelantunesjf@gmail.com>
2025-04-05 21:17:41 -0300

    feat: implementacao da rotina toggleDone e localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import TaskInput from "./components/TaskInput"
 import TaskList from "./components/TaskList"
 
 function App() {
 
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const addTask = (task) => {
-
     //id, title, done
     setTasks([...tasks, { id: Date.now(), text: task, done: false}]);
-
-
-    //localStorage
-
   }
 
   const deleteTask = (taskId) => {
     setTasks(tasks.filter((task) => task.id !== taskId));
   }
 
+  const toggleTaskDone = (taskId) => {
+    setTasks(
+      tasks.map(
+        (task) => task.id === taskId 
+        ? {...task, done: !task.done} 
+        : task
+      )
+    )
+  }
+
   return (
     <>
       <h1>Lista de tarefas</h1>
       <TaskInput onAddTask={addTask}/>
-      <TaskList tasks={tasks} onDeleteTask={deleteTask}/>
+      <TaskList tasks={tasks} onDeleteTask={deleteTask} onToggleTaskDone={toggleTaskDone}/>
     </>
   )
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,9 +1,14 @@
 import Task from './Task'
 
-const TaskList = ({tasks, onDeleteTask}) => {
+const TaskList = ({tasks, onDeleteTask, onToggleTaskDone}) => {
+
+  if(tasks.length === 0){
+    return <p>Não há tarefas cadastradas.</p>
+  } 
+
   return <ul>
     {tasks.map((task) => (
-        <Task key={task.id} task={task} onDelete={() => onDeleteTask(task.id)}/>
+        <Task key={task.id} task={task} onDelete={() => onDeleteTask(task.id)} onToggleDone={() => onToggleTaskDone(task.id)}/>
     ))}
   </ul>
 }
